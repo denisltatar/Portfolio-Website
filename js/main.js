6d2be93d8788ac8673b48ad5716a57f813c7c07b
Denis Tatar <denis.tatar8@gmail.com>
2020-04-06 23:37:50 -0400

    Updating my projects section.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 
 $(document).ready(function () {
+    // New Projects Stuff
+ // portfolio
+//  $('.gallery ul li a').click(function() {
+//     var itemID = $(this).attr('href');
+//     $('.gallery ul').addClass('item_open');
+//     $(itemID).addClass('item_open');
+//     return false;
+// });
+
+// $('.close').click(function() {
+//     $('.port, .gallery ul').removeClass('item_open');
+//     return false;
+// });
+
+// $(".gallery ul li a").click(function() {
+//     $('html, body').animate({
+//         scrollTop: parseInt($("#top").offset().top)
+//     }, 400);
+// });
+
+
 
     let $btns = $('.project-area .button-group button');
 
