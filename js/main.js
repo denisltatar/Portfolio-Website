ddb1ceb298d9fd6e5a4a303f6e9c3091f8c8c6e3
Denis Tatar <denis.tatar8@gmail.com>
2020-04-09 19:02:33 -0400

    New edits on projects section. Created all projects to be buttons.

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
     //     }, 400);
     // });
 
-    // Adding a Pop-Up
+    // Adding a Pop-Up (Project 1)
     // Get the modal
     var modal = document.getElementById("myModal");
 
@@ -46,6 +46,46 @@ $(document).ready(function () {
             modal.style.display = "none";
         }
     }
+
+
+
+
+
+    // Adding a Pop-Up (Project 2)
+    // Get the modal
+    var modal2 = document.getElementById("myModal2");
+
+    // Get the button that opens the modal
+    var btn2 = document.getElementById("myBtn2");
+
+    // Get the <span> element that closes the modal
+    var span2 = document.getElementsByClassName("close2")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn2.onclick = function () {
+        modal2.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span2.onclick = function () {
+        modal2.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
