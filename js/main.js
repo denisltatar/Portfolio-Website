1bfc3b8c8598150c7211589969a70a81bfff9d5b
Denis Tatar <denis.tatar8@gmail.com>
2020-04-08 23:38:02 -0400

    Created a new project section with buttons.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,51 @@
 
 $(document).ready(function () {
     // New Projects Stuff
- // portfolio
-//  $('.gallery ul li a').click(function() {
-//     var itemID = $(this).attr('href');
-//     $('.gallery ul').addClass('item_open');
-//     $(itemID).addClass('item_open');
-//     return false;
-// });
+    // portfolio
+    //  $('.gallery ul li a').click(function() {
+    //     var itemID = $(this).attr('href');
+    //     $('.gallery ul').addClass('item_open');
+    //     $(itemID).addClass('item_open');
+    //     return false;
+    // });
 
-// $('.close').click(function() {
-//     $('.port, .gallery ul').removeClass('item_open');
-//     return false;
-// });
+    // $('.close').click(function() {
+    //     $('.port, .gallery ul').removeClass('item_open');
+    //     return false;
+    // });
 
-// $(".gallery ul li a").click(function() {
-//     $('html, body').animate({
-//         scrollTop: parseInt($("#top").offset().top)
-//     }, 400);
-// });
+    // $(".gallery ul li a").click(function() {
+    //     $('html, body').animate({
+    //         scrollTop: parseInt($("#top").offset().top)
+    //     }, 400);
+    // });
+
+    // Adding a Pop-Up
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 
 
 
@@ -32,7 +59,7 @@ $(document).ready(function () {
 
         let selector = $(e.target).attr('data-filter');
         $('.project-area .grid').isotope({
-            filter : selector
+            filter: selector
         });
 
         return false;
