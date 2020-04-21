f61d1b49e6b20ace1221fff205ca939ba0f9de84
Denis Tatar <denis.tatar8@gmail.com>
2020-04-20 20:41:29 -0400

    Added navigation hover effect while scrolling.

--- js/main.js
+++ js/main.js
@@ -435,22 +435,24 @@ $(document).ready(function () {
 
         return false;
     })
-});
+
 
 // Sticky Navigation Menu
-// let nav_offset_top = $('.header_area').height() + 50;
+let nav_offset_top = $('.header_area').height() + 50;
 
-// function navbarFixed(){
-//     if($('.header_area').length){
-//         $(window).scroll(function(){
-//             let scroll = $(window).scrollTop();
-//             if (scroll >= nav_offset_top){
-//                 $('.header_area .main-menu').addClass('navbar_fixed');
-//             } else {
-//                 $('.header_area .main-menu').removeClass('navbar_fixed');
-//             }
-//         })
-//     }
-// }
+function navbarFixed(){
+    if($('.header_area').length){
+        $(window).scroll(function(){
+            let scroll = $(window).scrollTop();
+            if (scroll >= nav_offset_top){
+                $('.header_area .main-menu').addClass('navbar_fixed');
+            } else {
+                $('.header_area .main-menu').removeClass('navbar_fixed');
+            }
+        })
+    }
+}
 
-// navbarFixed();
+ navbarFixed();
+
+});
