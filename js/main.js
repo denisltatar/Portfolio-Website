f1bb59132ae282f5e994e57075d29279a583e49f
Denis Tatar <denis.tatar8@gmail.com>
2020-04-26 19:19:09 -0400

    Added another design project, Chief Wolf Clothing.

--- js/main.js
+++ js/main.js
@@ -409,6 +409,31 @@ $(document).ready(function () {
         }
     }
 
+    // Adding a Pop-Up (Project 9) Chief Wolf Clothing
+    // Get the modal
+    var modald9 = document.getElementById("myModald9");
+    // Get the button that opens the modal
+    var btnd9 = document.getElementById("myBtnd9");
+    // Get the <span> element that closes the modal
+    var span9 = document.getElementsByClassName("closed9")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd9.onclick = function () {
+        modald9.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand9.onclick = function () {
+        modald9.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald9) {
+            modald9.style.display = "none";
+        }
+    }
+
 
 
 
